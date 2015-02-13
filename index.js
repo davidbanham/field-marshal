2bc1b09fcca5a2c569962ba6773cf76c44dbe9e4
David Banham <david@banham.id.au>
2015-02-13 15:22:37 +1100

    Update shim for new coffee version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
-require('coffee-script')
+// Include the CoffeeScript interpreter so that .coffee files will work
+var coffee = require('coffee-script');
+
+// Explicitly register the compiler if required. This became necessary in CS 1.7
+if (typeof coffee.register !== 'undefined') coffee.register();
 require('./index.coffee')
